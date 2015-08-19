53fa8268f8b083ac89f1ae875fb10ebd5e7c88bc
Christopher Yang <cyang001@citymail.cuny.edu>
2015-08-19 00:11:33 -0400

    Add carousel slider feature for projects

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+var main = function(){
     $(".jumper").on("click", function (e) {
 
         e.preventDefault();
@@ -8,4 +8,49 @@ $(document).ready(function () {
         }, 500);
 
     });
-});
+    
+    $('.arrow-next').click(function(e){
+        e.preventDefault();
+        
+        var currentSlide = $('.active-slide');
+        var nextSlide = currentSlide.next()
+        
+        var currentDot = $('.active-dot');
+        var nextDot = currentDot.next();
+        
+        if(nextSlide.length == 0){
+            nextSlide = $('.slide').first();
+            nextDot = $('.dot').first();    
+        }
+        
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        nextSlide.fadeIn(600).addClass('active-slide');
+        
+        currentDot.removeClass('active-dot');
+        nextDot.addClass('active-dot');
+    });
+    
+    $('.arrow-prev').click(function(e) {
+        e.preventDefault();
+        
+        var currentSlide = $('.active-slide');
+        var prevSlide = currentSlide.prev();
+        
+        var currentDot = $('.active-dot');
+        var prevDot = currentDot.prev();
+
+        if(prevSlide.length == 0) {
+            prevSlide = $('.slide').last();
+            prevDot = $('.dot').last(); 
+        }
+
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        prevSlide.fadeIn(600).addClass('active-slide'); 
+        
+        currentDot.removeClass('active-dot');
+        prevDot.addClass('active-dot');
+    });
+    
+}
+
+$(document).ready(main);
